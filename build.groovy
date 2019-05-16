2b7940540f1b24f70b742ac9d90fd7c7cb601b02
Mohinder Kumar <mohkumar1@publicisgroupe.net>
2019-05-16 15:39:48 +0530

    New

--- build.groovy
+++ build.groovy
@@ -3,11 +3,12 @@ stage ('Clone') {
 	git url: 'https://github.com/jenkinsorganize/mytest.git'
 }
 stage('commit') {
+	withCredentials([usernamePassword(credentialsId: 'bfa8e939-5f84-4232-a623-ce5d0dd36d14', passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) { 
 	sh '''
 	touch README
 	git add .
 	git commit -m "README"
-	git push
+	git push https://${GIT_USER}:${GIT_PASS}@github.com/jenkinsorganize/mytest.git
 	'''
 }
 }
