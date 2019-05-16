e51272173f9f6fe99ad8b855a6b52d6627267995
Mohinder Kumar <mohkumar1@publicisgroupe.net>
2019-05-16 14:12:46 +0530

    new groovy

--- build.groovy
+++ build.groovy
@@ -2,7 +2,7 @@ def pipeline
 node {
 	stage ('Checkout') {
 println "starting checkout"
-["git", "checkout", "development"].execute()
+git url: 'https://github.com/jfrogdev/project-examples.git'
 }
 }
 
