c3d8759f5d9eb7133ead2322a09835a21e67710e
Mohinder Kumar <mohkumar1@publicisgroupe.net>
2019-05-16 15:30:40 +0530

    New

--- build.groovy
+++ build.groovy
@@ -1,5 +1,14 @@
 node {
 stage ('Clone') {
-git url: 'https://github.com/jenkinsorganize/newtest.git'
+	git url: 'https://github.com/jenkinsorganize/newtest.git'
 }
+stage('commit') {
+def getGitCommit() {
+    git_commit = sh (
+        script: 'git rev-parse HEAD',
+        returnStdout: true
+    ).trim()
+    return git_commit
+}
+
 }
