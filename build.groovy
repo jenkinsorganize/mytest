74ed0ae2140dfed667f47cb62dabca480adc799d
Mohinder Kumar <mohkumar1@publicisgroupe.net>
2019-05-16 12:59:16 +0530

    new groovy

--- build.groovy
+++ build.groovy
@@ -1,10 +1,4 @@
 #!groovy
-@groovy.transform.BaseScript com.ibm.dbb.groovy.ScriptLoader baseScript
-import com.ibm.dbb.build.*
-import com.ibm.dbb.repository.*
-import com.ibm.dbb.dependency.*
-import java.nio.file.*
-import groovy.time.*
 println "starting checkout"
 ["git", "checkout", "master"].execute()
 
