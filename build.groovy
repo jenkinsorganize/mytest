e8c4879fbb6d78fdebe43cfa70ad754b1a48d5e2
Mohinder Kumar <mohkumar1@publicisgroupe.net>
2019-05-16 15:53:02 +0530

    New

--- build.groovy
+++ build.groovy
@@ -3,12 +3,12 @@ stage ('Clone') {
 	git url: 'https://github.com/jenkinsorganize/mytest.git'
 }
 stage('commit') {
-	withCredentials([usernamePassword(credentialsId: 'bfa8e939-5f84-4232-a623-ce5d0dd36d14', passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) { 
+	withCredentials([usernamePassword(credentialsId: 'GitHub', passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) { 
 	sh '''
 	echo hello >> README
 	git add .
 	git commit -m "README"
-	git push --set-upstream origin master
+	git push --set-upstream https://${GIT_USER}:${GIT_PASS}@github.com/jenkinsorganize/mytest.git master
 	'''
 	}
 }
